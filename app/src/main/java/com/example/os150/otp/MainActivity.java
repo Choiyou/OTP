6b00bc1570e5208e228a8131170188bf77b89e83
Choiyou <os1502@naver.com>
2020-05-18 22:33:34 +0900

    TitleBar 제거 / Application Icon 변경

--- app/src/main/java/com/example/os150/otp/MainActivity.java
+++ app/src/main/java/com/example/os150/otp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.os150.otp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ActionBar 숨기기
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+
     }
 }
